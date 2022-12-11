24c03bab36a2a02ea2ae3040310a474bc7306abe
ahmet <locaahmet@gmail.com>
2022-12-12 01:02:23 +0300

    arrow functions

---
+++ lesson-3/index.js
@@ -0,0 +1,18 @@
+//Arrow Functions
+
+/*
+
+
+*/
+
+console.log(topla(3, 6));
+
+function topla(sayi1, sayi2) {
+  return sayi1 + sayi2;
+}
+
+console.log(topla2(10, 10));
+
+const topla2 = (sayi1, sayi2) => {
+  return sayi1 + sayi2;
+};
