3bb2a5b445836f632eeda0d5bd7f98fa1080910c
ahmet <locaahmet@gmail.com>
2022-12-12 01:29:13 +0300

    parameter defaults

---
+++ lesson-5/index.js
@@ -0,0 +1,27 @@
+// Parameter Defaults
+
+function topla(sayi1, sayi2) {
+  return sayi1 + sayi2; // 3 + undefined
+}
+
+console.log(topla(3)); // NaN
+
+//Parameter defaults
+
+function topla2(sayi1, sayi2 = 4) {
+  return sayi1 + sayi2; // 3 + 4
+}
+
+console.log(topla2(3)); // NaN
+
+/*
+
+React'ta reducer olustururken state'te baslangıc state'ti giriyoruz ve bunu orada kullanıyoruz
+*/
+
+
+const STATE_BASLANGIC = { yukleniyor : false, yazilar:[]};
+
+const reducer =(state = STATE_BASLANGIC, action)=>{ // State degerine baslangic olarak STATE_BASLANGIC degeri atadik.
+    return state;
+}
