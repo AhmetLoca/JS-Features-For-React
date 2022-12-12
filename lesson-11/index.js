176cae3fa74aa97320031733ecaf97890b928ffa
ahmet <locaahmet@gmail.com>
2022-12-12 13:15:28 +0300

    includes() and toLowerCase()

---
+++ lesson-11/index.js
@@ -0,0 +1,29 @@
+//.includes()
+
+const isim = "Mehmet";
+
+console.log(isim.includes("mehmet")); // false
+
+/*
+includes, buyuk kucuk harfe duyarli.
+*/
+
+//.toLowerCase()
+
+const yeniIsim = isim.toLowerCase();
+
+console.log(yeniIsim.includes("mehmet")); // true
+
+const urunler = [
+  { id: 1, name: "Kalem", fiyat: 5 },
+  { id: 2, name: "Defter", fiyat: 5 },
+  { id: 3, name: "Silgi", fiyat: 3 },
+  { id: 4, name: "Kalemtiras", fiyat: 7 },
+];
+
+const arama = "KALEM";
+console.log(
+  urunler.filter((urun) =>
+    urun.name.toLocaleLowerCase().includes(arama.toLowerCase())
+  )
+);
